489205f9fd95b48b916d60c20108f77226493278
Saurav <sauravads123@gmail.com>
2021-08-09 16:41:42 +0545

    test : without destroy policy

--- infra/modules/beanstalk_application/main.tf
+++ infra/modules/beanstalk_application/main.tf
@@ -2,16 +2,28 @@
 terraform {
   required_version = ">= v1.0.3"
 }
-resource "aws_iam_role" "aws_iam_role" {
-  
-}
+# resource "aws_iam_role" "aws_iam_role" {
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Action = "sts:AssumeRole"
+#         Effect = "Allow"
+#         Sid    = ""
+#         Principal = {
+#           Service = "ec2.amazonaws.com"
+#         }
+#       },
+#     ]
+#   })
+# }
 resource "aws_elastic_beanstalk_application" "beanstalk_app" {
   name        = var.aws_elastic_beanstalk_application_name
   description = var.aws_elastic_beanstalk_application_description
 
-  appversion_lifecycle {
-    service_role          = aws_iam_role.beanstalk_service.arn
-    max_count             = 128
-    delete_source_from_s3 = true
-  }
+  # appversion_lifecycle {
+  #   service_role          = aws_iam_role.beanstalk_service.arn
+  #   max_count             = 128
+  #   delete_source_from_s3 = true
+  # }
 }
