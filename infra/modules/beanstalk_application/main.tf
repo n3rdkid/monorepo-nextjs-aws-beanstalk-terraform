0a82b78bca93f9020a4f4ea29cf09ad48d271eab
Saurav <sauravads123@gmail.com>
2021-08-10 11:38:52 +0545

    test : testing deployment

--- infra/modules/beanstalk_application/main.tf
+++ infra/modules/beanstalk_application/main.tf
@@ -2,21 +2,26 @@
 terraform {
   required_version = ">= v1.0.3"
 }
-data "aws_iam_policy_document" "service" {
-  statement {
-    actions = [
-      "sts:AssumeRole"
-    ]
-    principals {
-      type        = "Service"
-      identifiers = ["elasticbeanstalk.amazonaws.com"]
-    }
-    effect = "Allow"
-  }
-}
+
 resource "aws_iam_role" "beanstalk_service" {
-  name               = "eb-service"
-  assume_role_policy = data.aws_iam_policy_document.service.json
+  name = "beeanstalk_service"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [{
+    "Effect": "Allow",
+    "Principal": {
+      "Service": "elasticbeanstalk.amazonaws.com"
+    },
+    "Action": "sts:AssumeRole",
+    "Condition": {
+      "StringEquals": {
+        "sts:ExternalId": "elasticbeanstalk"
+      }
+    }
+  }]
+}
+EOF
 }
 resource "aws_elastic_beanstalk_application" "beanstalk_app" {
   name        = var.aws_elastic_beanstalk_application_name
--- infra/modules/beanstalk_application/terraform.tf
+++ infra/modules/beanstalk_application/terraform.tf
@@ -1,3 +1,4 @@
+
 variable "aws_elastic_beanstalk_application_name" {
   type=string
   description = "Name of the Beanstalk application"
@@ -6,4 +7,9 @@ variable "aws_elastic_beanstalk_application_name" {
 variable "aws_elastic_beanstalk_application_description" {
   type=string
   description = "Description for the Beanstalk application"
+}
+variable "beanstalk_application_service_role" {
+  type = string
+  description = "Service role for the Beanstalk application"
+  
 }
