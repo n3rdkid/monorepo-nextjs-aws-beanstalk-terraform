3f8d687786fe6a35f4d943d1c4120a907ae3d9ed
Saurav <sauravads123@gmail.com>
2021-08-10 10:40:45 +0545

    test : deployment

--- infra/main.tf
+++ infra/main.tf
@@ -6,13 +6,13 @@ module "beanstalk_app_project" {
 
 module "beanstalk_environment_consumer_dev" {
       source = "./modules/beanstalk_environment"
-      aws_elastic_beanstalk_application_name = var.aws_elastic_beanstalk_application_name
+      aws_elastic_beanstalk_application_name = module.beanstalk_app_project.beanstalk_application_name
       beanstalk_environment_name = "consumer-dev"   
       beanstalk_environment_solution_stack_name=var.beanstalk_environment_solution_stack_name
 }
 module "beanstalk_environment_admin_dev" {
       source = "./modules/beanstalk_environment"
-      aws_elastic_beanstalk_application_name = var.aws_elastic_beanstalk_application_name
+      aws_elastic_beanstalk_application_name = module.beanstalk_app_project.beanstalk_application_name
       beanstalk_environment_name = "admin-dev"     
       beanstalk_environment_solution_stack_name=var.beanstalk_environment_solution_stack_name
 }
--- infra/modules/beanstalk_application/main.tf
+++ infra/modules/beanstalk_application/main.tf
@@ -2,28 +2,29 @@
 terraform {
   required_version = ">= v1.0.3"
 }
-# resource "aws_iam_role" "aws_iam_role" {
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Action = "sts:AssumeRole"
-#         Effect = "Allow"
-#         Sid    = ""
-#         Principal = {
-#           Service = "ec2.amazonaws.com"
-#         }
-#       },
-#     ]
-#   })
-# }
+data "aws_iam_policy_document" "service" {
+  statement {
+    actions = [
+      "sts:AssumeRole"
+    ]
+    principals {
+      type        = "Service"
+      identifiers = ["elasticbeanstalk.amazonaws.com"]
+    }
+    effect = "Allow"
+  }
+}
+resource "aws_iam_role" "beanstalk_service" {
+  name               = "eb-service"
+  assume_role_policy = data.aws_iam_policy_document.service.json
+}
 resource "aws_elastic_beanstalk_application" "beanstalk_app" {
   name        = var.aws_elastic_beanstalk_application_name
   description = var.aws_elastic_beanstalk_application_description
 
-  # appversion_lifecycle {
-  #   service_role          = aws_iam_role.beanstalk_service.arn
-  #   max_count             = 128
-  #   delete_source_from_s3 = true
-  # }
+  appversion_lifecycle {
+    service_role          = aws_iam_role.beanstalk_service.arn
+    max_count             = 128
+    delete_source_from_s3 = true
+  }
 }
