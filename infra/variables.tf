ab9265d7c3f5a6a2bff1f26d4b58c455cccf3d5b
Saurav <sauravads123@gmail.com>
2021-08-27 10:34:25 +0545

    minor improvements

---
+++ infra/variables.tf
@@ -0,0 +1,10 @@
+variable "aws_elastic_beanstalk_application_name" {
+}
+
+variable "aws_elastic_beanstalk_application_description" {
+}
+
+variable "beanstalk_environment_solution_stack_name" {
+}
+variable "beanstalk_environment_security_group" {
+}
